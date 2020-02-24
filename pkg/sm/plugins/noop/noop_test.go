82f011e1132afaa1a56d05c3618a7c95b6995798
Mamduh Alassi <mamduhala@mellanox.com>
2020-02-24 09:36:42 +0200

    Add unit test for noop plugin

---
+++ pkg/sm/plugins/noop/noop_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("noop plugin", func() {
+	Context("Initialize", func() {
+		It("Initialize noop plugin", func() {
+			plugin, err := Initialize(nil)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(plugin).ToNot(BeNil())
+			Expect(plugin.Name()).To(Equal("noop"))
+			Expect(plugin.Spec()).To(Equal("1.0"))
+			Expect(InvalidPlugin).ToNot(BeNil())
+
+			err = plugin.Validate()
+			Expect(err).ToNot(HaveOccurred())
+
+			err = plugin.AddGuidsToPKey(0, nil)
+			Expect(err).ToNot(HaveOccurred())
+
+			err = plugin.RemoveGuidsFromPKey(0, nil)
+			Expect(err).ToNot(HaveOccurred())
+		})
+	})
+})
