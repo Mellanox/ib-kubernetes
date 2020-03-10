5123f11aa7f0da4f5c39020c3db428094a85573f
Mamduh Alassi <mamduhala@mellanox.com>
2020-03-10 15:17:59 +0200

    Add periodic update

--- pkg/guid/guid_test.go
+++ pkg/guid/guid_test.go
@@ -2,6 +2,7 @@ package guid
 
 import (
 	"errors"
+	"github.com/Mellanox/ib-kubernetes/pkg/utils"
 
 	"github.com/Mellanox/ib-kubernetes/pkg/k8s-client/mocks"
 
@@ -57,28 +58,25 @@ var _ = Describe("GUID Pool", func() {
 			client := &mocks.Client{}
 			pods := &kapi.PodList{Items: []kapi.Pod{}}
 			pod1 := kapi.Pod{ObjectMeta: metav1.ObjectMeta{Name: "p1", Namespace: "default", Annotations: map[string]string{
-				v1.NetworkAttachmentAnnot: `[{"name":"test","cni-args":{"guid":"02:00:00:00:00:00:00:03"}}]`,
-			}}}
+				utils.InfiniBandAnnotation: `{"test3":"configured"}`,
+				v1.NetworkAttachmentAnnot:  `[{"name":"test3","cni-args":{"guid":"02:00:00:00:00:00:00:03"}}]`}}}
 			pod2 := kapi.Pod{ObjectMeta: metav1.ObjectMeta{Name: "p2", Namespace: "default", Annotations: map[string]string{
+				utils.InfiniBandAnnotation: `{"test":"configured", "test2":"configured"}`,
 				v1.NetworkAttachmentAnnot: `[{"name":"test","cni-args":{"guid":"02:00:00:00:00:00:00:04"}},
-                {"name":"test","cni-args":{"guid":"02:00:00:00:00:00:00:05"}}]`,
-			}}}
-			pod3 := kapi.Pod{ObjectMeta: metav1.ObjectMeta{Name: "p3", Namespace: "foo", Annotations: map[string]string{
-				v1.NetworkAttachmentAnnot: `[{"name":"test","namespace":"foo","cni-args":{"guid":"02:00:00:00:00:00:00:07"}}]`,
-			}}}
+                {"name":"test2","cni-args":{"guid":"02:00:00:00:00:00:00:05"}}]`}}}
+			pod3 := kapi.Pod{ObjectMeta: metav1.ObjectMeta{Name: "p3", Namespace: "default", Annotations: map[string]string{
+				utils.InfiniBandAnnotation: `{"test":"configured"}`,
+				v1.NetworkAttachmentAnnot:  `[{"name":"test","cni-args":{"guid":"02:00:00:00:00:00:00:03"}}]`}}}
 			pod4 := kapi.Pod{ObjectMeta: metav1.ObjectMeta{Name: "p4", Namespace: "foo", Annotations: map[string]string{
-				v1.NetworkAttachmentAnnot: `[{failed to parse}]`,
-			}}}
+				utils.InfiniBandAnnotation: `{"test":"configured"}`,
+				v1.NetworkAttachmentAnnot:  `[{"name":"test","namespace":"foo"}]`}}}
 			pod5 := kapi.Pod{ObjectMeta: metav1.ObjectMeta{Name: "p5", Namespace: "foo", Annotations: map[string]string{
-				v1.NetworkAttachmentAnnot: "not-json",
-			}}}
+				utils.InfiniBandAnnotation: `{}`,
+				v1.NetworkAttachmentAnnot:  `[{"name":"test","namespace":"foo"}]`}}}
 			pod6 := kapi.Pod{ObjectMeta: metav1.ObjectMeta{Name: "p6", Namespace: "foo", Annotations: map[string]string{
-				v1.NetworkAttachmentAnnot: "",
-			}}}
-			pod7 := kapi.Pod{ObjectMeta: metav1.ObjectMeta{Name: "p7", Namespace: "foo", Annotations: map[string]string{
-				"foo": "foo",
-			}}}
-			pod8 := kapi.Pod{ObjectMeta: metav1.ObjectMeta{Name: "p8", Namespace: "default"}}
+				utils.InfiniBandAnnotation: `invalid`,
+				v1.NetworkAttachmentAnnot:  `[{"name":"test","namespace":"foo"}]`}}}
+			pod7 := kapi.Pod{ObjectMeta: metav1.ObjectMeta{Name: "p7", Namespace: "foo"}}
 
 			pods.Items = append(pods.Items, pod1)
 			pods.Items = append(pods.Items, pod2)
@@ -87,7 +85,6 @@ var _ = Describe("GUID Pool", func() {
 			pods.Items = append(pods.Items, pod5)
 			pods.Items = append(pods.Items, pod6)
 			pods.Items = append(pods.Items, pod7)
-			pods.Items = append(pods.Items, pod8)
 
 			client.On("GetPods", "").Return(pods, nil)
 			pool, err := NewGuidPool("02:00:00:00:00:00:00:00",
@@ -111,87 +108,85 @@ var _ = Describe("GUID Pool", func() {
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(Equal("InitPool(): failed to get pods from kubernetes: err"))
 		})
-		It("Init pool failed to allocate invalid guid", func() {
-			client := &mocks.Client{}
-			pods := &kapi.PodList{Items: []kapi.Pod{}}
-			pod1 := kapi.Pod{ObjectMeta: metav1.ObjectMeta{Name: "p1", Namespace: "default", Annotations: map[string]string{
-				v1.NetworkAttachmentAnnot: `[{"name":"test","cni-args":{"guid":"invalid"}}]`,
-			}}}
-			pods.Items = append(pods.Items, pod1)
-
-			client.On("GetPods", "").Return(pods, nil)
-			pool, err := NewGuidPool("02:00:00:00:00:00:00:00",
-				"02:FF:FF:FF:FF:FF:FF:FF", client)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(pool).ToNot(BeNil())
-
-			err = pool.InitPool()
-			Expect(err).To(HaveOccurred())
-		})
-		It("Init pool failed to allocate allocated guid", func() {
-			client := &mocks.Client{}
-			pods := &kapi.PodList{Items: []kapi.Pod{}}
-			pod1 := kapi.Pod{ObjectMeta: metav1.ObjectMeta{Name: "p1", Namespace: "default", Annotations: map[string]string{
-				v1.NetworkAttachmentAnnot: `[{"name":"test","cni-args":{"guid":"02:00:00:00:00:00:00:00"}}]`,
-			}}}
-			pod2 := kapi.Pod{ObjectMeta: metav1.ObjectMeta{Name: "p2", Namespace: "default", Annotations: map[string]string{
-				v1.NetworkAttachmentAnnot: `[{"name":"test","cni-args":{"guid":"02:00:00:00:00:00:00:00"}}]`,
-			}}}
-			pods.Items = append(pods.Items, pod1)
-			pods.Items = append(pods.Items, pod2)
-
-			client.On("GetPods", "").Return(pods, nil)
-			pool, err := NewGuidPool("02:00:00:00:00:00:00:00",
-				"02:FF:FF:FF:FF:FF:FF:FF", client)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(pool).ToNot(BeNil())
-
-			err = pool.InitPool()
-			Expect(err).To(HaveOccurred())
-		})
 	})
-	Context("AllocateGUID", func() {
-		It("allocate guid when range is not full", func() {
+	Context("GenerateGUID", func() {
+		It("Generate guid when range is not full", func() {
 			pool, err := NewGuidPool("00:00:00:00:00:00:01:00", "00:00:00:00:00:00:01:01", nil)
 			Expect(err).ToNot(HaveOccurred())
-			guid, err := pool.AllocateGUID()
+			guid, err := pool.GenerateGUID("")
 			Expect(err).ToNot(HaveOccurred())
 			Expect(guid).To(Equal("00:00:00:00:00:00:01:00"))
-			guid, err = pool.AllocateGUID()
+			guid, err = pool.GenerateGUID("")
 			Expect(err).ToNot(HaveOccurred())
 			Expect(guid).To(Equal("00:00:00:00:00:00:01:01"))
 		})
-		It("Allocate and release guid then re-allocate the newly released guids", func() {
+		It("Generate and release guid then re-allocate the newly released guids", func() {
 			pool, err := NewGuidPool("00:00:00:00:00:00:01:00", "00:00:00:00:00:00:01:ff",
 				nil)
 			Expect(err).ToNot(HaveOccurred())
-			guid, err := pool.AllocateGUID()
+			guid, err := pool.GenerateGUID("")
 			Expect(err).ToNot(HaveOccurred())
 			Expect(guid).To(Equal("00:00:00:00:00:00:01:00"))
 			err = pool.ReleaseGUID(guid)
 			Expect(err).ToNot(HaveOccurred())
 
-			// allocate all the range
+			// Generate all the range
 			Expect(err).ToNot(HaveOccurred())
 			for i := 0; i < 255; i++ {
-				_, err = pool.AllocateGUID()
+				_, err = pool.GenerateGUID("pod" + string(i))
 				Expect(err).ToNot(HaveOccurred())
 			}
 
 			// After the last guid in the pool was allocated then the pool check back from first guid
-			guid, err = pool.AllocateGUID()
+			guid, err = pool.GenerateGUID("")
 			Expect(err).ToNot(HaveOccurred())
 			Expect(guid).To(Equal("00:00:00:00:00:00:01:00"))
 		})
-		It("allocate guid when range is full", func() {
+		It("Generate guid when current guid is allocated", func() {
+			p, err := NewGuidPool("00:00:00:00:00:00:01:00", "00:00:00:00:00:00:01:01", nil)
+			Expect(err).ToNot(HaveOccurred())
+			err = p.AllocateGUID("", "00:00:00:00:00:00:01:00")
+			Expect(err).ToNot(HaveOccurred())
+
+			guid, err := p.GenerateGUID("")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(guid).To(Equal("00:00:00:00:00:00:01:01"))
+		})
+		It("Generate guid when range is full", func() {
 			pool, err := NewGuidPool("00:00:00:00:00:00:01:00", "00:00:00:00:00:00:01:00", nil)
 			Expect(err).ToNot(HaveOccurred())
-			guid, err := pool.AllocateGUID()
+			guid, err := pool.GenerateGUID("")
 			Expect(err).ToNot(HaveOccurred())
 			Expect(guid).To(Equal("00:00:00:00:00:00:01:00"))
-			guid, err = pool.AllocateGUID()
+			guid, err = pool.GenerateGUID("")
 			Expect(err).To(HaveOccurred())
 			Expect(guid).To(Equal(""))
+		})
+	})
+	Context("AllocateGUID", func() {
+		It("Allocate guid from the pool", func() {
+			pool := &guidPool{guidPoolMap: map[string]bool{}, guidPodNetworkMap: map[string]string{}}
+			err := pool.AllocateGUID("", "02:00:00:00:00:00:00:00")
+			Expect(err).ToNot(HaveOccurred())
+		})
+		It("Allocate an allocated guid from the pool for the same pod", func() {
+			pool := &guidPool{guidPoolMap: map[string]bool{}, guidPodNetworkMap: map[string]string{}}
+			err := pool.AllocateGUID("pod1", "02:00:00:00:00:00:00:00")
+			Expect(err).ToNot(HaveOccurred())
+			err = pool.AllocateGUID("pod1", "02:00:00:00:00:00:00:00")
+			Expect(err).ToNot(HaveOccurred())
+		})
+		It("Allocate an allocated guid from the pool", func() {
+			pool := &guidPool{guidPoolMap: map[string]bool{}, guidPodNetworkMap: map[string]string{}}
+			err := pool.AllocateGUID("pod1", "02:00:00:00:00:00:00:00")
+			Expect(err).ToNot(HaveOccurred())
+			err = pool.AllocateGUID("pod2", "02:00:00:00:00:00:00:00")
+			Expect(err).To(HaveOccurred())
+		})
+		It("Allocate invalid guid from the pool", func() {
+			pool := &guidPool{guidPoolMap: map[string]bool{}}
+			err := pool.AllocateGUID("", "invalid")
+			Expect(err).To(HaveOccurred())
 		})
 	})
 	Context("ReleaseGUID", func() {
