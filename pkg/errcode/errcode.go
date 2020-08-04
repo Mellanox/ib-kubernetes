4cbe96f968a89cf39a2513d4c0b9a269a71fb67f
Dmytro Linkin <dmitrolin@mellanox.com>
2020-08-04 13:57:22 +0300

    !fixes(3) pkg/errors: Implement custom error

--- pkg/errcode/errcode.go
+++ pkg/errcode/errcode.go
@@ -1,3 +1,29 @@
+// Package errcode defines the errCode type, which extend common error handling,
+// by providing error code value in addition to error message.
+//
+// To start using package, at first You need to implement desired error codes.
+// Example:
+//   const (
+//        ErrorUnknown = iota // NOTE: should start from 0
+//        ErrorFirst
+//        ...
+//        ErrorLast
+//   )
+//
+// To create new errCode with some text use `NewErr' method, with formatted
+// text - `Errorf' method.
+// Examples:
+//   err := errcode.NewErr(ErrorFirst, "Some text describing error")
+//   err := errcode.Errorf(ErrorLast, "Failed to do something. Error %d", value)
+//
+// To get error code value use `GetCode' method, text - `Error' method. Example:
+//   if errcode.GetCode(err) == ErrorUnknown {
+//        <do something>
+//        fmt.Println(err.Error())
+//   }
+//
+// For code examples refer to:
+// https://github.com/Mellanox/ib-kubernetes/blob/master/pkg/daemon/daemon.go
 package errcode
 
 import "fmt"
@@ -13,11 +39,12 @@ const (
 	NotErrCodeType = iota - 1
 )
 
+// Error returns error message.
 func (e *errCode) Error() string {
 	return e.text
 }
 
-// Return error code value or NotErrCodeType if variable isn't of type errCode
+// GetCode returns error code value or NotErrCodeType if variable isn't of type errCode.
 func GetCode(e error) int {
 	err, ok := e.(*errCode)
 	if !ok {
@@ -26,12 +53,12 @@ func GetCode(e error) int {
 	return err.code
 }
 
-// Create new errCode
+// NewErr creates new errCode with provided text.
 func NewErr(code int, text string) error {
 	return &errCode{code: code, text: text}
 }
 
-// Create new errCode with formated text.
+// Errorf creates new errCode with formated text.
 func Errorf(code int, format string, a ...interface{}) error {
 	return &errCode{code: code, text: fmt.Sprintf(format, a...)}
 }
